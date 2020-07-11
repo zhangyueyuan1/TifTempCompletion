abc6bb1b5ccf1919a738046f627b9a1292a235f2
FranklinZhang <franklinzhang@foxmail.com>
2020-07-12 01:40:19 +0800

    append findnull function
    Signed-off-by: FranklinZhang <franklinzhang@foxmail.com>

--- src/main.py
+++ src/main.py
@@ -4,6 +4,37 @@
 # Description: complete temperature data in Tiff
 
 import gdal
+import zyytif
 
 gdal.AllRegister()
 
+# Target Data
+target = "./test/A2018015_lst.tif"
+
+# Completion Data
+completion = ""
+
+# Vegedation Data
+vege = ""
+
+def findPairs(target, completion):
+    pass
+
+def calTthd():
+    pass
+
+def calVthd():
+    pass
+
+def findNullCell(target, nullValue):
+    dataset1 = gdal.Open(target)
+    band = dataset1.GetRasterBand(1)
+    buffer = band.ReadAsArray(0, 0, dataset1.RasterXSize, dataset1.RasterYSize)
+    nullcells = []
+    for rowIndex in range(len(buffer)):
+        for cellIndex in range(len(buffer[rowIndex])):
+            if buffer[rowIndex][cellIndex] == nullValue:
+                nullcells.append([rowIndex, cellIndex])
+    return nullcells
+
+ncells = findNullCell(target, 65535)
